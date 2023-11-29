b4f2d47013f7e1b0ebd9b630447ca36238038c87
Evheniy Ishchenko <herdid04@gmail.com>
2023-11-29 21:19:52 +0200

    Добавьте файлы проекта.

---
+++ Lab4/CommonFraction.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4;
+
+public class CommonFraction
+{
+    private int numerator;
+    private int denominator;
+
+    public CommonFraction(int numerator, int denominator)
+    {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.numerator}/{this.denominator}";
+    }
+
+    public CommonFraction reverseFraction()
+    {
+        return new CommonFraction(denominator, numerator).reducingFraction();
+    }
+
+    public static CommonFraction operator +(CommonFraction first, CommonFraction second)
+    {
+        int sumNumerator = first.numerator * second.denominator + second.numerator * first.denominator;
+        int sumDenominator = first.denominator * second.denominator;
+        return new CommonFraction(sumNumerator, sumDenominator).reducingFraction();
+    }
+
+    public static CommonFraction operator -(CommonFraction first, CommonFraction second)
+    {
+        int sumNumerator = first.numerator * second.denominator - second.numerator * first.denominator;
+        int sumDenominator = first.denominator * second.denominator;
+        return new CommonFraction(sumNumerator, sumDenominator).reducingFraction();
+    }
+
+    public static CommonFraction operator *(CommonFraction first, CommonFraction second)
+    {
+        int sumNumerator = first.numerator * second.numerator;
+        int sumDenominator = first.denominator * second.denominator;
+        return new CommonFraction(sumNumerator, sumDenominator).reducingFraction();
+    }
+
+    private CommonFraction  reducingFraction()
+    {
+        CommonFraction result = this;
+        for (int i = 9; i > 1; i--)
+        {
+            if ((result.numerator%i == 0) && (result.denominator % i == 0))
+            {
+                result.numerator = result.numerator / i;
+                result.denominator = result.denominator / i;
+            }
+        }
+        return result;
+    }
+}
+
---
+++ Lab4/Program.cs
@@ -0,0 +1,18 @@
+﻿using Lab4;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        CommonFraction fraction_1 = new(7, 4);
+        CommonFraction fraction_2 = new(8, 7);
+        CommonFraction fraction_3 = new(2, 4);
+        
+        CommonFraction R;
+
+        R = (fraction_1+ fraction_2)* (fraction_1 - fraction_3);
+
+        Console.WriteLine("Result ---> " + R);
+        Console.WriteLine("Reversed ---> " + R.reverseFraction());
+    }
+}
